d8bae5d96435e10d273820f282d4aafde70d93a6
deoren <deoren@users.noreply.github.com>
2018-10-04 13:57:35 -0500

    Prune unneeded files, add priv esc
    - Ansible playbook handles tasks previously carried out
      by Bash script
    
    - Adjust priv esc on db setup tasks

--- sql/setup_redmine_database.sql
+++
@@ -1,8 +0,0 @@
--- https://github.com/deoren/redmine-testing
-
--- NOTE: This should probably be utf8mb4 for full Unicode support
-CREATE DATABASE redmine_trunk CHARACTER SET utf8;
-
--- Matching values set within /path/to/redmine/config/database.yml
-CREATE USER 'redmine_trunk'@'localhost' IDENTIFIED BY 'redmine';
-GRANT ALL PRIVILEGES ON redmine_trunk.* TO 'redmine_trunk'@'localhost';
