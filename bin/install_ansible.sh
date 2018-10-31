fec7b2aff411d0a9e1421401422a954387349b71
deoren <deoren@users.noreply.github.com>
2018-10-31 09:44:56 -0500

    Ansible setup script: Add additional support pkgs
    Add several packages listed by the "Ansible for DevOps"
    book. At least one of these is needed for the work handled
    by the prototype playbook currently in development in this
    repo.
    
    - python-apt
    - python-pycurl
    - python-mysqldb

--- bin/install_ansible.sh
+++ bin/install_ansible.sh
@@ -5,8 +5,15 @@
 # Purpose: Install Ansible from Ubuntu PPA
 
 # https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-ubuntu
+# https://www.cyberciti.biz/faq/how-to-install-and-configure-latest-version-of-ansible-on-ubuntu-linux/
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
+
+# Install additional common support packages
+sudo apt-get install \
+    python-apt \
+    python-pycurl \
+    python-mysqldb \
